a32660a7bd2b09e7771732e6fdf85346b577a813
mbahsamuelchukwuemeka@outlook.com <mbahsamuelchukwuemeka@outlook.com>
2025-10-24 13:09:26 +0100

    fix create category dto with the the correct properties

--- DTOs/Category/CreateCategoryDTO.cs
+++ DTOs/Category/CreateCategoryDTO.cs
@@ -2,7 +2,8 @@
 {
     public class CreateCategoryDTO
     {
-        public string Name { get; set; } =string.Empty;
-        public string? Description { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string CategoryType { get; set; } = string.Empty;
+        //public ICollection<Transaction> Transactions { get; set; }
     }
 }
