e595322b8e9b9aedc99c782cf2a924e95580158e
mbahsamuelchukwuemeka@outlook.com <mbahsamuelchukwuemeka@outlook.com>
2025-11-25 19:24:19 +0100

    add a new controller to test live backend

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseVista.API.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            return Ok(new { message = "The api is working well" });
+        }
+    }
+}
